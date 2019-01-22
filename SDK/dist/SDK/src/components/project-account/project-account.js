87a56c5af37a54e200587345d5e5f5169ac05428
t.kandibolotskaya <t.kandibolotskaya@bet-makers.com>
2019-01-22 17:56:04 +0200

    fixes

--- SDK/dist/SDK/src/components/project-account/project-account.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=project-account.js.map
--- SDK/dist/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_URL = 'http://localhost:5000';
-//# sourceMappingURL=config.js.map
